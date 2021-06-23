95e35800c5550186d9ff540ce1e8f2154d3f029e
anthony <anthonytran.dev@gmail.com>
2021-06-23 20:41:26 +0100

    remove invert object util

--- lib/utils/invertObject.ts
+++
@@ -1,9 +0,0 @@
-const invertObject = (object: Record<string, number>) => {
-  var createdObject = {}
-  for (var key in object) {
-    createdObject[object[key]] = key
-  }
-  return createdObject
-}
-
-export default invertObject
